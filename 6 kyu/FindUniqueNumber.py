5e74c242552bab0f4c0c67816dc5e0ac718a6f52
matthewarogers <166161548+matthewarogers@users.noreply.github.com>
2024-04-06 10:14:28 -0500

    Update FindUniqueNumber.py

--- 6 kyu/FindUniqueNumber.py
+++ 6 kyu/FindUniqueNumber.py
@@ -2,5 +2,9 @@ def find_uniq(arr):
     #iterate through array and find different number
     base = arr[0]
     for i in arr:
-        if i != base:
+        #Check if unique is in first position
+        if arr[0] != arr[2] and arr[0] != arr[1]:
+            return arr[0]
+        #Else check the position of the unique value
+        elif i != base:
             return i
